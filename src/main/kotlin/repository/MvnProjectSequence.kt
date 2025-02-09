c1a71f74e10a17ba046df38ef8a336301f0149b5
kechin.vv <kechinv1@rambler.ru>
2025-02-09 05:41:35 +0300

    MvnProjectSequence v0 (not tested)

--- src/main/kotlin/repository/MvnProjectSequence.kt
+++ src/main/kotlin/repository/MvnProjectSequence.kt
@@ -2,6 +2,7 @@ package org.kechinvv.repository
 
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+import fj.data.Stream
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
@@ -12,12 +13,11 @@ import okhttp3.RequestBody.Companion.toRequestBody
 val SONATYPE_BODY_TEMPLATE = Any::class::class.java.getResource("/sonabody.json")!!.readText(Charsets.UTF_8)
 
 class MvnProjectSequence(val lib: String, val client: OkHttpClient) : Sequence<RemoteRepository> {
-    val linkSONATYPE = "https://central.sonatype.com/api/internal/browse/dependents"
+    private val LinkSonatype = "https://central.sonatype.com/api/internal/browse/dependents"
 
     private var page = 0
-    private var searchTerm = ""
+    private var searchTerm = Term()
 
-    val SLEEP: Long = 30_000
     private val pageSize = 20
     private val maxPageCount = 500
 
@@ -32,38 +32,81 @@ class MvnProjectSequence(val lib: String, val client: OkHttpClient) : Sequence<R
             }
             val response = makeRequest()
             page++
-            val json = JsonParser.parseString(response).asJsonObject
-            val reps = getReps(json)
+            val reps = getReps(response)
             reps
         }
     }.flatten()
 
     private fun endOfSearch(): Boolean =
-        endOfPages() && (searchTerm == "" && !needFilterByTerm() || searchTerm == "zzz")
+        endOfPages() && (searchTerm.isEmpty() && !needFilterByTerm() || searchTerm.isLast())
 
     private fun needFilterByTerm(): Boolean = foundPageCount == maxPageCount
     private fun endOfPages(): Boolean = page == foundPageCount
 
 
     private fun nextTerm() {
-        TODO()
+        searchTerm.next()
+        var pages = maxPageCount
+        while (pages == maxPageCount || pages == 0) {
+            val response = makeRequest()
+            pages = response.getAsJsonPrimitive("pageCount").asInt
+            if (pages == maxPageCount) searchTerm.expandTerm()
+            else if (pages == 0) searchTerm.next()
+        }
     }
 
-    private fun makeRequest(): String {
-        val queryUrlBuilder = linkSONATYPE.toHttpUrl().newBuilder()
+    private fun makeRequest(): JsonObject {
+        val queryUrlBuilder = LinkSonatype.toHttpUrl().newBuilder()
         val body = String.format(SONATYPE_BODY_TEMPLATE, lib, page, pageSize, searchTerm)
             .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
         val request = Request.Builder()
             .url(queryUrlBuilder.build())
             .post(body)
             .build()
-        return client.newCall(request).execute().body.string()
+        val response = client.newCall(request).execute().body.string()
+        return JsonParser.parseString(response).asJsonObject
     }
 
     private fun getReps(json: JsonObject): List<RemoteRepository> {
         foundPageCount = json.getAsJsonPrimitive("pageCount").asInt
-        return emptyList()
+        val jsonReps = json.getAsJsonArray("components")
+        val res = mutableListOf<RemoteRepository>()
+        jsonReps.forEach {
+            val item = it.asJsonObject
+            val namespace = item.get("sourceNamespace").asString
+            val name = item.get("sourceName").asString
+            val version = item.get("sourceVersion").asString
+            res.add(RemoteLib(namespace, name, version))
+        }
+        return res
     }
 
     override fun iterator() = inner.iterator()
+}
+
+private class Term {
+    val term: ArrayList<Char> = arrayListOf()
+
+    fun next() {
+        for (i in term.size - 1 downTo 0) {
+            if (term[i] != 'z') {
+                term[i] = term[i] + 1
+                break
+            } else {
+                term.dropLast(1)
+            }
+        }
+
+    }
+
+    fun expandTerm() {
+        term.add('a')
+    }
+
+    fun isEmpty(): Boolean = term.isEmpty()
+    fun isLast(): Boolean = term.fold(true) { acc, el -> acc && el == 'z' }
+
+    override fun toString(): String {
+        return term.joinToString(separator = "")
+    }
 }

135e391f6d4979e90e9bc8ce3617f6326ed71bd2
kechin.vv <kechinv1@rambler.ru>
2025-06-17 02:55:30 +0300

    clean main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,5 @@
 package org.kechinvv
 
-import org.kechinvv.config.Configuration
-import org.kechinvv.repository.JarLocalRepository
-import org.kechinvv.workflow.DefaultWorkflow
-import java.nio.file.Paths
-
 fun main() {
-    val config = Configuration()
-    config.targetLibExtractingUnit = setOf("okhttp", "okhttp3", "com.squareup.okhttp")
-    val repo = JarLocalRepository(
-        Paths.get("C:\\Users\\valer\\IdeaProjects\\LibMiner\\workdir_instr\\okhttpmvn\\aca-java-1.0.0\\aca-java-1.0.0.jar"),
-        Paths.get("C:\\Users\\valer\\IdeaProjects\\LibMiner\\workdir_instr\\okhttpmvn\\aca-java-1.0.0")
-    )
 
-    val defaultWorkflow = DefaultWorkflow(config)
-    defaultWorkflow.analyze(repo)
 }
